03c884f387a2feaee22a258d8e4b666dac624e49
Geovanni <gtinoco354@gmail.com>
2024-03-03 20:56:16 -0500

    Initial Commit

---
+++ SendIt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SendIt
+//
+//  Created by Geovanni Tinoco on 3/3/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
